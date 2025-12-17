895cfde9eb8671424611f632d24cad93c38ab3ca
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-17 11:05:35 +0530

    feat: many-to-many relationship
    Implemented many-to-many relationship.

--- src/main/java/com/social/media/models/SocialUser.java
+++ src/main/java/com/social/media/models/SocialUser.java
@@ -1,12 +1,17 @@
 package com.social.media.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
@@ -22,4 +27,12 @@ public class SocialUser {
 
     @OneToMany
     private List<Post> posts = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+        name = "user_group",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<SocialGroup> socialGroups = new HashSet<>();
 }
