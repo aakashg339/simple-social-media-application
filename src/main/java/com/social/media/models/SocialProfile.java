4b6629d0726e7e053a5802878cfc1d09ac958dcc
aakashg339 <141503528+aakashg339@users.noreply.github.com>
2025-12-17 10:07:14 +0530

    feat: one-to-one mapping
    Implemented one-to-one mapping between SocialProfile and SocialUser.

---
+++ src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,19 @@
+package com.social.media.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @OneToOne
+    @JoinColumn(name = "social_user")
+    private SocialUser user;
+}
